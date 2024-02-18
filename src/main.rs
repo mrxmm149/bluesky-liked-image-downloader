71a4a5dd2bd9dfcef27f2d5e913d1be24f78bef4
mrxmm149 <mrxmm149@gmail.com>
2024-02-18 16:28:18 +0900

    misc: initial commit
    Signed-off-by: mrxmm149 <mrxmm149@gmail.com>

---
+++ src/main.rs
@@ -0,0 +1,108 @@
+use std::error::Error;
+use std::io::{stdin, Read, Write};
+use std::path::Path;
+
+use atrium_api::app::bsky::feed::defs;
+use atrium_xrpc_client::reqwest::ReqwestClient;
+
+use chrono::{DateTime, Local};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let args: Vec<String> = std::env::args().collect();
+    let config_path = args.get(1).cloned().unwrap_or("config.toml".into());
+    let config: Config = toml::from_str(&std::fs::read_to_string(config_path)?)?;
+
+    let agent = atrium_api::agent::AtpAgent::new(
+        ReqwestClient::new(config.service_uri),
+        atrium_api::agent::store::MemorySessionStore::default(),
+    );
+    agent.login(config.login_id, config.login_pw).await?;
+
+    let params = atrium_api::app::bsky::feed::get_actor_likes::Parameters {
+        actor: config.target_user,
+        cursor: None,
+        limit: None,
+    };
+    let likes = agent.api.app.bsky.feed.get_actor_likes(params).await?.feed;
+
+    futures::future::join_all(
+        likes
+            .iter()
+            .map(|f| download_image(f, &config.download_directory)),
+    )
+    .await;
+
+    pause();
+
+    Ok(())
+}
+
+async fn download_image(
+    f: &defs::FeedViewPost,
+    download_directory: &String,
+) -> Result<(), Box<dyn Error>> {
+    let post = &f.post;
+
+    // filename: {author handle}/{created_at_localtime}_bsky_{post_id}_{image_no}.{ext}
+    // example: bsky.app/20231223_035832_bsky_3kh5rarr3gn2n_01.jpeg
+    // todo: make it customizable
+    let author_handle = post.author.handle.clone();
+
+    // Technically we should follow https://www.docs.bsky.app/docs/advanced-guides/timestamps#sortat
+    let creation_time = DateTime::parse_from_rfc3339(&post.indexed_at)?;
+    let creation_time_local: DateTime<Local> = DateTime::from(creation_time);
+    let creation_time_str = creation_time_local.format("%Y%m%d_%H%M%S");
+
+    let post_id = post.uri.rsplit_once('/').unwrap().1;
+
+    let mut urls: Vec<String> = Vec::new();
+    if let Some(view) = &post.embed {
+        if let defs::PostViewEmbedEnum::AppBskyEmbedImagesView(image) = view {
+            urls = image.images.iter().map(|i| i.fullsize.clone()).collect();
+        }
+    }
+
+    for (ii, url) in urls.iter().enumerate() {
+        let dir = Path::new(download_directory).join(&author_handle);
+        std::fs::create_dir_all(&dir)?;
+
+        let ext = url.rsplit_once('@').unwrap().1;
+        let file_name = format!(
+            "{}_bsky_{}_{:02}.{}",
+            creation_time_str,
+            post_id,
+            ii + 1,
+            ext
+        );
+        let path = dir.join(file_name);
+        download_file(&url, path.to_str().unwrap()).await?;
+    }
+
+    Ok(())
+}
+
+async fn download_file(url: &str, file_name: &str) -> Result<(), Box<dyn Error>> {
+    let content = reqwest::get(url).await?.bytes().await?;
+    let mut file = std::fs::File::create(file_name)?;
+    file.write_all(&content)?;
+    println!("Downloaded {}", file_name);
+    Ok(())
+}
+
+fn pause() {
+    // let mut stdout = stdout();
+    // stdout.write(b"Press Enter to continue...").unwrap();
+    // stdout.flush().unwrap();
+    println!("Press Enter to continue...");
+    stdin().read(&mut [0]).unwrap();
+}
+
+#[derive(serde::Deserialize)]
+struct Config {
+    service_uri: String,
+    login_id: String,
+    login_pw: String,
+    target_user: String,
+    download_directory: String,
+}
